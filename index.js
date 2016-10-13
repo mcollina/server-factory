61517b58a5e5a089a999ebb3f274a8747bcc46c4
Matteo Collina <hello@matteocollina.com>
2016-10-13 23:44:17 +0200

    Added tcp support

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const EE = require('events')
+const net = require('net')
+const steed = require('steed')
+
+function factory (list) {
+  const servers = new EE()
+  var instances = null
+
+  steed.map(list, create, function (err, i) {
+    if (err) {
+      return servers.emit('error', err)
+    }
+    instances = i
+  })
+
+  servers.close = function (cb) {
+    steed.map(instances, close, cb)
+  }
+
+  servers.addresses = function () {
+    if (!instances) {
+      return null
+    }
+
+    return instances.map((i) => i.address())
+  }
+
+  return servers
+
+  function create (opts, cb) {
+    switch (opts.protocol) {
+      case 'tcp':
+        createNet(opts, cb)
+        break
+      default:
+        cb(new Error('unknown protocol: ' + opts.protocol))
+    }
+  }
+
+  function createNet (opts, cb) {
+    const host = opts.hostname || opts.host
+    const port = opts.port || 0
+
+    const server = net.createServer(onStream)
+
+    server.listen(port, host, function () {
+      server.removeListener('error', cb)
+      cb(null, server)
+    })
+
+    server.on('error', cb)
+  }
+
+  function onStream (stream) {
+    servers.emit('stream', stream)
+  }
+
+  function close (server, cb) {
+    server.unref()
+    server.close(cb)
+  }
+}
+
+module.exports = factory
---
+++ test/tcp.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const net = require('net')
+const tap = require('tap')
+const test = tap.test
+const factory = require('..')
+
+const servers = factory([{
+  protocol: 'tcp',
+  port: 3033
+}])
+
+servers.on('stream', function (stream) {
+  // echo
+  stream.pipe(stream)
+})
+
+tap.tearDown(servers.close.bind(servers))
+
+test('basic connect', (t) => {
+  t.plan(1)
+
+  const stream = net.connect({ host: 'localhost', port: 3033 })
+
+  stream.on('data', function (data) {
+    t.equal(data.toString(), 'hello')
+    stream.end()
+  })
+
+  stream.write('hello')
+})
+
+test('connect with data from server', (t) => {
+  t.plan(1)
+
+  const stream = net.connect(servers.addresses()[0])
+
+  stream.on('data', function (data) {
+    t.equal(data.toString(), 'hello')
+    stream.end()
+  })
+
+  stream.write('hello')
+})
